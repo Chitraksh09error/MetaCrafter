19925a20adc5a8663083b183d35afb196a150302
Chitraksh09error <chitrakshchavan4@gmail.com>
2025-07-16 17:27:22 +0530

    server

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,19 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),  // <-- alias added
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  server: {
+    historyApiFallback: true, // ✅ Fix refresh on routes
+  },
+  build: {
+    rollupOptions: {
+      input: 'index.html', 
     },
   },
 });
